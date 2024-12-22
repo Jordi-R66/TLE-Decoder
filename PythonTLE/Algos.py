733f94be2a863841be5a2b8a23e67d5687d9b401
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2024-12-23 00:09:45 +0100

    Fixed some formatting issues

--- PythonTLE/Algos.py
+++ PythonTLE/Algos.py
@@ -24,18 +24,19 @@ def AntecedentDroite(a: float, b: float, y: float) -> float:
 	return (y - b) / a
 
 def NewtonRaphson(target, func_param, func, func_prime, x_start: float, tolerance: float, max_iter: int) -> float:
-	xf=0
-	x_guess=x_start;
+	xf = 0
+	x_guess = x_start
 
-	low_limit = target - tolerance;
-	high_limit = target + tolerance;
+	low_limit = target - tolerance
+	high_limit = target + tolerance
 
 	n: int = 0
 
-	while (n<max_iter) and not ((low_limit <= func(x_guess, func_param)) and (func(x_guess, func_param) <= high_limit)):
-		a, b = func_prime(x_guess, func_param), -func_prime(x_guess, func_param)*x_guess + func(x_guess, func_param);
-		x_guess = AntecedentDroite(a, b, target);
-		n+=1
+	while (n < max_iter) and not ((low_limit <= func(x_guess, func_param)) and (func(x_guess, func_param) <= high_limit)):
+		a, b = func_prime(x_guess, func_param), -func_prime(x_guess, func_param)*x_guess + func(x_guess, func_param)
+		x_guess = AntecedentDroite(a, b, target)
+		n += 1
 
-	xf = x_guess;
+	xf = x_guess
+
 	return xf
