f5170f98d9fabb7b6b8cae5e4a48ce08cdbb9140
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2024-12-25 16:55:15 +0100

    Started implementing download via space track

--- download_tle.py
+++ download_tle.py
@@ -1,7 +1,9 @@
+from fileinput import filename
+from h11 import Response
 from requests import get
 from time import time, time_ns
 
-SETS: list[str] = sorted([
+CELESTRAK_SETS: list[str] = sorted([
 	"active",
 	"stations",
 	"last-30-days",
@@ -58,19 +60,40 @@ SETS: list[str] = sorted([
 	"other"
 ])
 
-BASE_URL: str = "https://celestrak.org/NORAD/elements/gp.php?GROUP=SET_NAME&FORMAT=tle"
+SPACETRACK_BULK: str = "https://www.space-track.org/basicspacedata/query/class/gp/decay_date/null-val/epoch/%3Enow-30/orderby/norad_cat_id/format/3le"
+BASE_CELESTRAK_URL: str = "https://celestrak.org/NORAD/elements/gp.php?GROUP=SET_NAME&FORMAT=tle"
 
-def download_set(set_name: str):
+def download_spacetrack():
 	START: float = time_ns()
-	REQ_URL: str = BASE_URL.replace("SET_NAME", set_name)
-	req = get(REQ_URL)
+
+	req = get(SPACETRACK_BULK)
+
+	response_bytes: bytes = req.content
+	response_encoding: str = req.apparent_encoding
+
+	response: str = response_bytes.decode("ascii")
+
+	filename: str = "bulk.tle"
+
+	lines: list[str] = response.split("\n")
+	print(req.status_code)
+
+	for i in range(len(lines)):
+		lines[i] = lines[i][2::]
+		print(lines[i])
+
+def download_celestrak_set(set_name: str):
+	START: float = time_ns()
+
+	REQ_URL: str = BASE_CELESTRAK_URL.replace("SET_NAME", set_name)
+	req: Response = get(REQ_URL)
 
 	if ("not found" in req.text):
 		print(f"Downloading {set_name} as SPECIAL...")
 		REQ_URL = REQ_URL.replace("GROUP", "SPECIAL")
 		req = get(REQ_URL)
 
-	response_bytes = req.content
+	response_bytes: bytes = req.content
 
 	filename: str = f"TLEs/{set_name}.tle"
 
@@ -84,12 +107,15 @@ def download_set(set_name: str):
 def download_all():
 	total_time: int = 0
 	exec_time: int = 0
-	for set_name in SETS:
+
+	for set_name in CELESTRAK_SETS:
 		print(f"Downloading {set_name}")
-		exec_time = download_set(set_name)
+
+		exec_time = download_celestrak_set(set_name)
 		print(f"{set_name} succesfully downloaded in {exec_time/1_000_000_000:.3}s !")
+
 		total_time += exec_time
-	
+
 	print(f"All files have been downloaded in {total_time/1_000_000_000:.3}s")
 
 def merge_all():
@@ -100,34 +126,40 @@ def merge_all():
 	total_time: int = 0
 	exec_time: int = 0
 
-	for set_name in SETS:
+	for set_name in CELESTRAK_SETS:
 		print(f"Merging {set_name}.tle")
+
 		START: int = time_ns()
+
 		filename = f"TLEs/{set_name}.tle"
+
 		current_file = open(filename, "rb")
 		lines = current_file.readlines()
+
 		current_file.close()
 
 		n_block = len(lines)//3
-		
+
 		for i in range(n_block):
 			block = lines[i*3: i*3+3]
 			NORAD_ID = int(block[2][2:7])
-			
+
 			if (NORAD_ID not in merged_ids) and (b"UNKNOWN" not in block[0]):
 				merged_ids.append(NORAD_ID)
 				final_file.writelines(block)
+
 		END: int = time_ns()
 
 		exec_time = END - START
 		total_time += exec_time
 
 		print(f"{set_name}.tle merged in {exec_time/1_000_000_000:.3}s")
-	
+
 	print(f"All files have been merged in {total_time/1_000_000_000:.3}s")
 
 	final_file.close()
 
 if __name__ == "__main__":
+	# download_spacetrack()
 	download_all()
 	merge_all()
