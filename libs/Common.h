1aff5a5459e637e779da82d523f130d3de0590b7
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-05 21:15:36 +0100

    Ajout de la gestion conditionnelle de l'inclusion de <stdbool.h> et ajout de <unistd.h> pour Linux

--- libs/Common.h
+++ libs/Common.h
@@ -4,8 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <math.h>
 #include <time.h>
+
+#ifdef __linux__
+	#include <unistd.h>
+#endif
+
+#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
+/* bool, true and false are keywords.  */
+#else
+#include <stdbool.h>
+#endif
 
 typedef char string;
