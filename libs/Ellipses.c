1877a02850ca30188b1927f34bbf3b5004c20958
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-05 23:13:44 +0100

    Renommer les paramètres des fonctions d'ellipses pour améliorer la lisibilité

--- libs/Ellipses.c
+++ libs/Ellipses.c
@@ -1,21 +1,21 @@
 #include "Ellipses.h"
 
-double semiMinorAxis(double semiMajorAxis, double eccentricity) {
-	return semiMajorAxis * sqrt(1.0 - pow(eccentricity, 2.0));
+double b(double a, double e) {
+	return a * sqrt(1.0 - pow(e, 2.0));
 }
 
-double c(double semiMajorAxis, double eccentricity) {
-	return semiMajorAxis * eccentricity;
+double c(double a, double e) {
+	return a * e;
 }
 
-double h(double semiMajorAxis, double eccentricity) {
-	return p(semiMajorAxis, eccentricity) / eccentricity;
+double h(double a, double e) {
+	return p(a, e) / e;
 }
 
-double f(double semiMajorAxis, double eccentricity) {
-	return semiMajorAxis / eccentricity;
+double f(double a, double e) {
+	return a / e;
 }
 
-double p(double semiMajorAxis, double eccentricity) {
-	return semiMajorAxis * (1.0 - pow(eccentricity, 2.0));
+double p(double a, double e) {
+	return a * (1.0 - pow(e, 2.0));
 }
