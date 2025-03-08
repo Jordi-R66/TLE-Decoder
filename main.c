237c04c476d9d2dfc8cc4023d0f061a99ee0d8dd
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-08 18:48:07 +0100

    Ajout de calculs pour la longitude de périgée et mise à jour de l'affichage des coordonnées et de l'altitude dans PrintTle

--- main.c
+++ main.c
@@ -94,6 +94,16 @@ void PrintTle(TLE Object, bool debug) {
 
 	double longPeri = (Object.AscNodeLong + Object.PeriArg) * DEGS2RADS;
 
+	longPeri *= RADS2DEGS;
+	longPeri -= (double)((uint32_t)(longPeri / 360.0) * 360);
+	longPeri *= DEGS2RADS;
+
+	double AscNodeTA = Object.AscNodeLong * DEGS2RADS - longPeri;
+	double AscNodeR = OrbAltTA(Object.Eccentricity, SMA, AscNodeTA);
+
+	KeplerCoords2D_t AscNode = coordsFromTA(AscNodeR, AscNodeTA);
+	KeplerCoords3D_t AscNode3D = { AscNode.x, AscNode.y, 0.0 };
+
 	double Epoch_E_Approx = (Object.MeanAnomaly * DEGS2RADS) + Object.Eccentricity * sin(Object.MeanAnomaly * DEGS2RADS);
 	double Epoch_E = NewtonRaphson(Object.MeanAnomaly * DEGS2RADS, Object.Eccentricity, *KeplerEquation, *KeplerPrime, Epoch_E_Approx, EccentricAnomalyTolerance, DEFAULT_ITER);
 	double Epoch_TA = TrueAnomaly(Object.Eccentricity, Epoch_E);
@@ -159,14 +169,13 @@ void PrintTle(TLE Object, bool debug) {
 		Current_TA -= (double)((uint32_t)(Current_TA / 360.0) * 360);
 
 		KeplerCoords2D_t focal = FocalRelativeToBaricenter(SMA, Object.Eccentricity);
-		KeplerCoords2D_t coords_2d = basic2DKeplerCoords(SMA, Object.Eccentricity, Current_E);
+		KeplerCoords2D_t coords_2d = coordsFromTA(Current_R, Current_TA * DEGS2RADS);//basic2DKeplerCoords(SMA, Object.Eccentricity, Current_E);
 
-		coords_2d = PointRelativeToFocal(focal, coords_2d);
+		KeplerCoords2D_t bariRelCoords = sumCoords2D(focal, coords_2d);
 
-		//focal = Rotate2DKeplerCoords(focal, longPeri);
-		//coords_2d = Rotate2DKeplerCoords(coords_2d, longPeri);
 
-		
+
+		//coords_2d = PointRelativeToFocal(focal, coords_2d);
 
 		double altitude = sqrt(pow(coords_2d.x, 2) + pow(coords_2d.y, 2)) - EARTH_RADIUS;
 
@@ -186,6 +195,7 @@ void PrintTle(TLE Object, bool debug) {
 
 		printf("INCLINATION : %.4lf degs\n", Object.Inclination);
 		printf("LONGITUDE OF ASC. NODE : %.4f degs\n", Object.AscNodeLong);
+		printf("LONGITUDE OF PERIAPSIS : %.4f degs\n", longPeri * RADS2DEGS);
 		printf("ECCENTRICITY : %.7lf\n", Object.Eccentricity);
 		printf("ARG. OF PERIAPSIS : %.4lf degs\n", Object.PeriArg);
 		printf("MEAN ANOMALY : %.4lf degs\n", Object.MeanAnomaly);
@@ -204,25 +214,15 @@ void PrintTle(TLE Object, bool debug) {
 		// printf("MEAN ANOMALY : %.4lf degs\n", Current_MA);
 		// printf("ECC. ANOMALY : %.4lf rads\n", Current_E);
 		// printf("TRUE ANOMALY : %.4lf degs\n", Current_TA);
-		printf("X Coord : %.2lf m\tX Earth : %.2lf m\n", coords_2d.x, focal.x);
-		printf("Y Coord : %.2lf m\tY Earth : %.2lf m\n", coords_2d.y, focal.y);
+		printf("X Coord : %.0lf m\tX Earth : %.0lf m\n", coords_2d.x, focal.x);
+		printf("Y Coord : %.0lf m\tY Earth : %.0lf m\n", coords_2d.y, focal.y);
 		// printf("Z Coord : %.2lf m\tZ Speed : %.2lf m/s\n", Coord3D_Z, Speed3D_Z);
-		printf("ALTITUDE : %.2lf km\n", Current_Alt / 1000);
-		printf("ALTITUDE : %.2lf km\n", altitude / 1000);
-		printf("SPEED : %.2lf km/s\n", Current_Spd / 1000);
-		printf("Ap : %.2lf km | Pe : %.2lf km\n", keplerDistance(SMA, Object.Eccentricity, M_PI) - EARTH_RADIUS, keplerDistance(SMA, Object.Eccentricity, .0) - EARTH_RADIUS);
+		printf("ALTITUDE : %.0lf m\n", Current_Alt);
+		printf("ALTITUDE : %.0lf m\n", altitude);
+		printf("SPEED : %.2lf m/s\n", Current_Spd);
+		//printf("Ap : %.2lf km | Pe : %.2lf km\n", keplerDistance(SMA, Object.Eccentricity, M_PI) - EARTH_RADIUS, keplerDistance(SMA, Object.Eccentricity, .0) - EARTH_RADIUS);
 
-		/*
-		deallocMatrix(&ArgPeriRot);
-		deallocMatrix(&IncliRot);
-		deallocMatrix(&ANRot);
-
-		deallocMatrix(&OrbCoords2D);
-		deallocMatrix(&RefCoords3D);
-
-		deallocMatrix(&OrbSpeed2D);
-		deallocMatrix(&RefSpeed3D);
-		*/
+		printf("-------------------------------------------------------------------------\n");
 	} else {
 
 		uint64_t orb_period = ((uint64_t)OrbitalPeriod(Object.MeanMotion) + 2);
