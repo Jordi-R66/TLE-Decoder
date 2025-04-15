b8d63fdc0641cbfb8e2da662cdf13410514d0250
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-15 21:37:45 +0200

    Ajout du type de retour double à la fonction getCoord dans vectors.h

--- ext_libs/vectors.h
+++ ext_libs/vectors.h
@@ -7,7 +7,7 @@ typedef Matrix Vector;
 void allocVector(Vector* vector, size_t coords);
 void deallocVector(Vector* vector);
 
-void getCoord(Vector* vector, size_t coordNumber);
+double getCoord(Vector* vector, size_t coordNumber);
 void setCoord(Vector* vector, size_t coordNumber, double value);
 
 Vector crossProduct(Vector* vectorA, Vector* vectorB);
