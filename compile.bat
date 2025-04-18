c756a765430a549e80b2f87735a3cfd304ae11d2
Jordi-R66 <jordirocafort66@gmail.com>
2025-04-18 20:23:47 +0200

    Mise à jour du script de compilation pour corriger le nom de l'exécutable et ajuster les options de compilation

--- compile.bat
+++ compile.bat
@@ -1,8 +1,7 @@
-@echo off
 cls
-set COMPILER=gcc.exe
+set COMPILER=gcc
 
-set SOURCES=libs/TleFiles.c libs/OrbMaths.c libs/TleParser.c libs/CONVERSIONS.c libs/Algos.c libs/TermFuncs.c main.c
+set SOURCES=libs/TleFiles.c libs/OrbMaths.c libs/TleParser.c libs/CONVERSIONS.c libs/Algos.c libs/TermFuncs.c libs/Kepler.c libs/Ellipses.c ext_libs/myOwnCLib/matrices/matrix.c ext_libs/myOwnCLib/vectors/vectors.c main.c
 
 set OUTPUT_DIR=output/
 
@@ -10,6 +9,7 @@ mkdir %OUTPUT_DIR%
 
 set EXEC=TLE.exe
 
-set ARGS=-O3 -std=c17 -m64 -march=x86-64 -flto
+set ARGS=-g -O0 -std=c23
+REM set ARGS=-O3 -std=c17 -m64 -march=x86-64 -flto
 
 %COMPILER% %ARGS% %SOURCES% -o %OUTPUT_DIR%%EXEC%
