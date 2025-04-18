0336f8e94ba027db18f403f198f16f8fc58382a9
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-18 14:34:43 +0200

    Réorganisation des inclusions dans Common.h pour garantir l'inclusion de bool.h de myOwnCLib et suppression de la vérification de la version de C.

--- libs/Common.h
+++ libs/Common.h
@@ -9,14 +9,10 @@
 #include <math.h>
 #include <time.h>
 
+#include "../ext_libs/myOwnCLib/bools/bool.h"
+
 #ifdef __linux__
 	#include <unistd.h>
-#endif
-
-#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
-/* bool, true and false are keywords.  */
-#else
-#include <stdbool.h>
 #endif
 
 typedef char string;
