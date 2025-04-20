1d9bc52560616e5ce06db5c02e2ef5b3fd9fc83c
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-20 19:31:28 +0200

    Suppression des fonctions d'exportation et d'importation de fichiers de structure dans TleFiles.c et TleFiles.h

--- libs/TleFiles.h
+++ libs/TleFiles.h
@@ -6,7 +6,3 @@ int32_t GetTLENumber(string* filename);
 TLE* GetAllTle(string* filename);
 
 void PrintContentAsAscii(string* filename);
-
-void ExportToStructFile(TLE* tle_list, int32_t tle_number, string* filename);
-
-TLE* ImportFromStructFile(string* filename, int32_t* tle_number);
