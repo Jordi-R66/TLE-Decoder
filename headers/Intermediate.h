63ab160dd68a261d1268564ab7517c9ce9fe58de
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-21 14:03:40 +0200

    Ajout des définitions de structures OrbitData et EpochData dans Intermediate.h pour le calcul des données orbitales.

--- headers/Intermediate.h
+++ headers/Intermediate.h
@@ -10,4 +10,32 @@
 #define DEFAULT_ITER 100000
 #define CALENDAR_YEAR 365.25
 
+typedef struct OrbitData {
+	double OrbPeriod;
+	double SMA;
+	double n;
+
+	double Ap, Pe;
+	double longPeri;
+	double epoch;
+} OrbitData;
+
+typedef struct EpochData {
+	double E;
+	double MA;
+	double TA;
+
+	double R;
+	double Alt;
+	double Spd;
+
+	KeplerCoords2D_t coords2D;
+	KeplerCoords3D_t coords3D;
+} EpochData;
+
+#define ORBIT_DATA_LENGTH sizeof(OrbitData)
+#define EPOCH_DATA_LENGTH sizeof(EpochData)
+
+OrbitData computeOrbitData(TLE Object);
+
 void PrintTle(TLE Object);
