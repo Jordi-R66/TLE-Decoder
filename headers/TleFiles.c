e6140388794d7312ee9c48ab0538ec78b09d44a9
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-22 00:32:26 +0200

    Added a function to retrieve only a single TLE, thus reducing memory usage

--- headers/TleFiles.c
+++ headers/TleFiles.c
@@ -25,7 +25,6 @@ int32_t GetTLENumber(string* filename) {
 TLE* GetAllTle(string* filename) {
 
 	uint32_t TLE_NUMBER = GetTLENumber(filename);
-	const uint8_t block_size = 165;
 
 	FILE* tle_file = fopen(filename, "r");
 
@@ -92,6 +91,74 @@ TLE* GetAllTle(string* filename) {
 	fclose(tle_file);
 
 	return Output;
+}
+
+TLE GetSingleTLE(string* filename, uint32_t NORAD_ID) {
+	uint32_t TLE_NUMBER = GetTLENumber(filename);
+	FILE* tle_file = fopen(filename, "r");
+
+	TLE Output;
+	memset(&Output, 0, sizeof(Output));
+
+	string first_line[25];
+	string second_line[70];
+	string third_line[70];
+
+	for (uint32_t block_number = 0; block_number < TLE_NUMBER; block_number++) {
+
+		uint8_t block_line = 0;
+		uint8_t j = 0;
+
+		for (uint8_t i = 0; i < block_size; i++) {
+			uint32_t pos = block_number * block_size + i;
+
+			if (i >= 25) {
+				block_line = 1;
+				j = i - 25;
+			}
+
+			if (i >= 95) {
+				block_line = 2;
+				j = i - 95;
+			}
+
+			fseek(tle_file, pos, SEEK_SET);
+			char c = fgetc(tle_file);
+
+			if ((c == '\n') || (c == EOF)) {
+				c = '\0';
+			}
+
+			switch (block_line) {
+				case 0:
+					first_line[j] = c;
+					break;
+
+				case 1:
+					second_line[j] = c;
+					break;
+
+				case 2:
+					third_line[j] = c;
+					break;
+
+				default:
+					break;
+			}
+			j++;
+		}
+
+		Output = parse_lines(first_line, second_line, third_line);
+
+		if (Output.NORAD_ID == NORAD_ID) {
+			fclose(tle_file);
+			return Output;
+		}
+	}
+
+	fclose(tle_file);
+	fprintf(stderr, "Error: Couldn't find %u in %s\n", NORAD_ID, filename);
+	exit(EXIT_FAILURE);
 }
 
 void PrintContentAsAscii(string* filename) {
--- headers/TleFiles.h
+++ headers/TleFiles.h
@@ -5,6 +5,7 @@
 int32_t GetTLENumber(string* filename);
 
 TLE* GetAllTle(string* filename);
+TLE GetSingleTLE(string* filename, uint32_t NORAD_ID);
 
 void PrintContentAsAscii(string* filename);
 
