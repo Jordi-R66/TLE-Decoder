0bac45e9f0d82553b3ffa7929bfe44a9edde7f2c
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-02-21 15:32:58 +0100

    Ajout de constantes physiques dans le programme

---
+++ headers/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define SIDEREAL_DAY 86164.096	// Earth sidereal day in seconds
+#define MEAN_SOLAR_DAY 86400	// Earth mean solar day in seconds
+
+#define G 6.67428e-11
+
+#define MU_EARTH 3.986004415e14	// Earth µ (TT)
+#define EARTH_MASS MU_EARTH / G	// Earth mass
