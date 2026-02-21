b306f09f8ebf10aeb6f732a4184d75b3caeb98a2
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-02-21 15:32:13 +0100

    Fixed include problems on math.h

--- headers/Common.h
+++ headers/Common.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// Active les constantes mathématiques (M_PI) de <math.h> sous Windows (MSVC)
+#define _USE_MATH_DEFINES 
+// Active les extensions POSIX et constantes mathématiques sous Linux (glibc)
+#define _GNU_SOURCE
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
--- headers/Conversions.h
+++ headers/Conversions.h
@@ -12,9 +12,6 @@ double strtoscinotd(char*);
 
 string secstohms(double secs);
 
-#undef M_PI
-#define M_PI 3.141592653589793
-
 #define DEGS2RADS M_PI/180.0
 #define RADS2DEGS 180.0/M_PI
 
