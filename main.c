e22677cf70ac0d6398e78faf9c3442d8908c4c25
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-02-23 15:54:24 +0100

    Création d'une fonction pour l'affichage des données après calcul

--- main.c
+++ main.c
@@ -1,12 +1,36 @@
 #include "headers/utils.h"
 #include "headers/DynamicPhase.h"
 
+static char epoch_string[DATE_STRING_LENGTH] = {0};
+static char instant_string[DATE_STRING_LENGTH] = {0};
+
+static char age_string[DELTA_TIME_STRING_LENGTH] = {0};
+
 TLE initPhase(char* filename, uint32_t noradId) {
 	FILE* fp = fopen(filename, "r");
 	TLE tle = GetSingleTLE(fp, noradId);
 	fclose(fp);
 
 	return tle;
+}
+
+void printValues(TLE tle, StaticValues init, DynamicValues instant) {
+	time_t instant_ts = init.epoch_timestamp + instant.deltaTime;
+
+	timestampToDateString(instant_ts, instant_string);
+	deltaTimeToString(instant.deltaTime, age_string);
+
+	printf("%lu\n", instant.deltaTime);
+
+	printf("Object name : %s\n", tle.name);
+	printf("\n");
+	printf("NORAD ID : %u%c\n", tle.NORAD_ID, tle.Classification);
+	printf("COSPAR : %u %hu %s\n", tle.COSPAR_YR, tle.COSPAR_LN, tle.COSPAR_ID);
+	printf("EPOCH : %s\n", epoch_string);
+	printf("TLE AGE : %s\n", age_string);
+	printf("(MEAN MOTION)' = \n");
+	printf("(MEAN MOTION)'' = \n");
+	printf("B* = \n");
 }
 
 int main(int argc, char** argv) {
@@ -16,18 +40,15 @@ int main(int argc, char** argv) {
 	TLE tle = initPhase(filename, noradId);
 	StaticValues staticVals = staticPhase(tle);
 
-	char now_string[DATE_STRING_LENGTH];
+	timestampToDateString(staticVals.epoch_timestamp, epoch_string);
 
 	while (true) {
 		time_t current_ts = time(NULL);
 		DynamicValues dynVals = dynamicPhase(tle, staticVals, current_ts);
 
-		timestampToDateString(current_ts, now_string);
-
 		clear_screen();
 
-		printf("%s\nnu: %.5e rads\nDist: %.5f meters\n", now_string, dynVals.true_ano, dynVals.distanceToFocal);
-		printf("X: %lf m\nY: %lf m\n", dynVals.coords_2d.x, dynVals.coords_2d.y);
+		printValues(tle, staticVals, dynVals);
 
 		sleep_ms(20); // Pause 20ms soit 50Hz
 	}
