e98a3f290cac0c4256db923d92061d88a9eeb3a5
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-02-23 14:04:15 +0100

    Ajout d'un champ distanceToFocal (altitude du satellite) ET début implémentation fonction dynamicPhase

--- headers/DynamicPhase.c
+++ headers/DynamicPhase.c
@@ -2,3 +2,25 @@
 
 #include "Algos.h"
 #include "Kepler.h"
+
+DynamicValues dynamicPhase(TLE tle, StaticValues init, time_t currentTimestamp) {
+	DynamicValues output = { 0 };
+
+	time_t deltaTime = currentTimestamp - init.epoch_timestamp;
+
+	double deltaM = (double)(init.n) * deltaTime;
+	double M = fmod(deltaM + init.M, 2.0 * M_PI);
+
+	double E = EccentricAnomaly(M, tle.Eccentricity);
+	double nu = TrueAnomaly(E, tle.Eccentricity);
+	double r = AltFromTA(init.a, tle.Eccentricity, nu);
+
+	output.ecc_ano = E;
+	output.mean_ano = M;
+	output.nu = nu;
+	output.distanceToFocal = r;
+
+	output.deltaTime = currentTimestamp;
+
+	return output;
+}
--- headers/DynamicPhase.h
+++ headers/DynamicPhase.h
@@ -5,6 +5,8 @@
 typedef struct dynamicValues {
 	time_t deltaTime;
 	double nu, mean_ano, ecc_ano;
+
+	double distanceToFocal;
 } DynamicValues;
 
 DynamicValues dynamicPhase(TLE tle, StaticValues init, time_t currentTimestamp);
