82f0871d7c1672973e5d3054ded086aef3e06163
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-02-26 07:27:03 +0100

    Déclaration de fonctions et types dans Aggreg.h

---
+++ headers/Aggreg.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "utils.h"
+#include "DynamicPhase.h"
+
+typedef struct {
+	TLE tle;
+	DynamicValues values_at_time;
+	StaticValues init;
+	time_t timestamp;
+} AggregValues;
+
+#define AGGREG_VALS_SIZE sizeof(AggregValues)
+
+AggregValues getAggregAtTimestamp(char* filename, uint32_t norad_id, time_t timestamp);
+void changeAggregTimestamp(AggregValues* aggreg, time_t timestamp);
+
+// Type pour les fonctions chargées d'écrire les données dans un flux
+// Doit retourner un void, prendre en entrée un pointeur vers une struct FILE et une struct AggregValues
+typedef void (*AggregWriter)(FILE*, AggregValues);
